390dce92e9605d5682324f88b0a377a03c7e8c77
fopefoluwa <ojodorcas57@gmail.com>
2025-08-12 16:10:45 +0100

    Assignment added

---
+++ week_2/Tasks_4.py
@@ -0,0 +1,16 @@
+#favourite life quote
+Quote = input("Enter favourite quote :")
+# converted_quote = Quote.title()
+print(f"\'{Quote}\'".title())
+
+
+
+# #Shopping List Manager
+empty_list =[]
+shopping_items_1 = input("Kindly input shopping items:")
+empty_list.append(shopping_items_1)
+shopping_items_2 = input("Kindly input shopping items:")
+empty_list.append (shopping_items_2)
+shopping_items_3 = input("Kindly input shopping items:")
+empty_list.append (shopping_items_3)
+print(empty_list)
