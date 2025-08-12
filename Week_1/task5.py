2034e513241af9bb8030171ee861b1c75ac3ecdc
fopefoluwa <ojodorcas57@gmail.com>
2025-08-12 15:25:00 +0100

    modified a file

--- Week_1/task5.py
+++ Week_1/task5.py
@@ -1,5 +1,5 @@
 #Daily Market Report 
-# market_name =input("Enter your market name: ")
+market_name = input("Enter your market name: ")
 numbers_of_traders = input("Enter number of traders: ")
 Daily_revenue =input("Enter daily revenue: ")
 print(f"The market {market_name} has {numbers_of_traders} and earn {Daily_revenue} daily")
